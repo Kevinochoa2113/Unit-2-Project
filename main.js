e6b746eb64449c0ac15c7ca9b98e45d73b4e388b
Fernando Nevarez <david.nevarez03@hotmail.com>
2020-09-10 17:58:31 -0700

    Made a new branch

---
+++ main.js
@@ -0,0 +1,9 @@
+let decimalNumber = prompt("Whhats the decimal number?");
+
+let binaryNumber = 
+
+let hexadecimalNumber =
+
+console.log(`The binary of ${decimalNumber} is ${binaryNumber}`);
+
+console.log(`The hexadecimal of ${decimalNumber} is ${hexadecimalNumber}`);
