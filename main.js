1a785910e2ff98a573632cddc3df50aecf4c52ad
Fernando Nevarez <david.nevarez03@hotmail.com>
2020-09-11 11:41:25 -0700

    Updated javascript content

--- main.js
+++ main.js
@@ -3,30 +3,55 @@
 let decimalNumber = prompt("What is the decimal number?");
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-let binaryNumber =  decimalNumber / 2)
+firstRemainder1 = Math.floor(decimalNumber / 2);
+
+firstRemainder2 = (Math.ceil(decimalNumber / 2) - (firstRemainder1));
+
+secondRemainder1 = Math.floor(firstRemainder1 / 2);
+
+secondRemainder2 = (Math.ceil(firstRemainder1 / 2) - (secondRemainder1));
+
+thirdRemainder1 = Math.floor(secondRemainder1 / 2);
+
+thirdRemainder2 = (Math.ceil(secondRemainder1 / 2) - (thirdRemainder1));
+
+fourthRemainder1 = Math.floor(thirdRemainder1 / 2);
+
+fourthRemainder2 = (Math.ceil(thirdRemainder1 / 2) - (fourthRemainder1));
+
+
+
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-let hexadecimalNumber =
+let hexadecimalRemainder1 = decimalNumber / 16
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-if(remainder1 == 10){
-    remainder1 == "A";
-}else if(remainder1 == 11){
-    remainder1 == "B";
-}else if(remainder1 == 12){
-    remainder1 == "C";
-}else if(remainder1 == 13){
-    remainder1 == "D";
-}else if(remainder1 == 14){
-    remainder1 == "E";
-}else if(remainder1 == 15){
-    remainder1 == "F";
+let hexadecimalRemainder2 = hexadecimalRemainder1 * 16
+
+hexadecimalNumber = hexadecimalRemainder1 + hexadecimalRemainder2
+
+
+if(hexadecimalRemainder1 == 10){
+    hexadecimalRemainder1 == "A";
+}else if(hexadecimalRemainder1 == 11){
+    hexadecimalRemainder1 == "B";
+}else if(hexadecimalRemainder1 == 12){
+    hexadecimalRemainder1 == "C";
+}else if(hexadecimalRemainder1 == 13){
+    hexadecimalRemainder1 == "D";
+}else if(hexadecimalRemainder1 == 14){
+    hexadecimalRemainder1 == "E";
+}else if(hexadecimalRemainder1 == 15){
+    hexadecimalRemainder1 == "F";
+}else(hexadecimalRemainder1 > 10 ){
+    hexadecimalRemainder1 == hexadecimalRemainder1;
 }
 
 
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 console.log(`The binary of ${decimalNumber} is ${binaryNumber}`);
 
 console.log(`The hexadecimal of ${decimalNumber} is ${hexadecimalNumber}`);
+
