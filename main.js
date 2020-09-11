46a5f0c7c6a57fe43ce49e8eed9ddd832c21bcc6
Fernando Nevarez <david.nevarez03@hotmail.com>
2020-09-10 18:11:58 -0700

    Updated javascript branch

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-let decimalNumber = prompt("Whhats the decimal number?");
 
-let binaryNumber = 
+
+let decimalNumber = prompt("What is the decimal number?");
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let binaryNumber =  decimalNumber / 2)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 let hexadecimalNumber =
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if(remainder1 == 10){
+    remainder1 == "A";
+}else if(remainder1 == 11){
+    remainder1 == "B";
+}else if(remainder1 == 12){
+    remainder1 == "C";
+}else if(remainder1 == 13){
+    remainder1 == "D";
+}
+
+
+
 
 console.log(`The binary of ${decimalNumber} is ${binaryNumber}`);
 
