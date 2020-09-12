912aaebbe605c7c474c1b3b881c060706c83667a
Fernando Nevarez <david.nevarez03@hotmail.com>
2020-09-11 18:28:56 -0700

    updated javascript branch

--- main.js
+++ main.js
@@ -21,15 +21,15 @@ fourthRemainder2 = (Math.ceil(thirdRemainder1 / 2) - (fourthRemainder1));
 
 */
 
-if (firstRemainder1 > 0){
+if (firstRemainder1 >= 0){
 
-    firstRemainder1 = Math.floor(decimalNumber / 2);
-    
-    firstRemainder2 = (Math.ceil(decimalNumber / 2) - (firstRemainder1));
-}else{
+    secondRemainder1Answer = Math.floor(decimalNumber / 2);
+
+    secondRemainder2Answer = (Math.ceil(decimalNumber / 2) - (firstRemainder1));
 
 }
 
+console.log(`${secondRemainder2Answer}`);
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 let hexadecimalRemainder1 = decimalNumber / 16
@@ -51,14 +51,28 @@ if(hexadecimalRemainder1 == 10){
     hexadecimalRemainder1 == "E";
 }else if(hexadecimalRemainder1 == 15){
     hexadecimalRemainder1 == "F";
-}else(hexadecimalRemainder1 > 10 ){
+}else{
     hexadecimalRemainder1 == hexadecimalRemainder1;
+}
+
+if(hexadecimalRemainder2 == 10){
+    hexadecimalRemainder2 == "A";
+}else if(hexadecimalRemainder2 == 11){
+    hexadecimalRemainder2 == "B";
+}else if(hexadecimalRemainder2 == 12){
+    hexadecimalRemainder2 == "C";
+}else if(hexadecimalRemainder2 == 13){
+    hexadecimalRemainder2 == "D";
+}else if(hexadecimalRemainder2 == 14){
+    hexadecimalRemainder2 == "E";
+}else if(hexadecimalRemainder2 == 15){
+    hexadecimalRemainder2 == "F";
 }
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-console.log(`The binary of ${decimalNumber} is ${binaryNumber}`);
+console.log(`The binary of ${decimalNumber} is ${secondRemainder2Answer}`);
 
 console.log(`The hexadecimal of ${decimalNumber} is ${hexadecimalNumber}`);
 
