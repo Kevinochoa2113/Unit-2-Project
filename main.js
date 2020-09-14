57a888dd27e00ba6bd8d7a4cf003320dc2ad8707
Fernando Nevarez <david.nevarez03@hotmail.com>
2020-09-14 16:06:24 -0700

    updated javascript content

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /*This program will ask for a decimal number, then it will convert that given decimal number into a binary and hexadecimal number.*/
 let decimalNumber = Number(prompt("What is the decimal number?"));
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* This is the part of the code that will solve to convert the given decimal number into a binary number*/
-//this part will divide the decimal number by 2, to get the remainder for the answer later on. Will also keep on divide the answer to "...Remainder1"
+//this part will divide the decimal number by 2, to get the remainder for the answer later on. Will also keep on divide the given decimal by two to get the answer to "...Remainder2" and then display the answer in the console.
 firstRemainder1 = Math.floor(decimalNumber / 2);
 
 firstRemainder2 = (Math.ceil(decimalNumber / 2) - (firstRemainder1));
@@ -39,7 +39,8 @@ ninthRemainder1 = Math.floor(eighthRemainder1 / 2);
 
 ninthRemainder2 = (Math.ceil(eighthRemainder1 / 2) - (ninthRemainder1));
 
-//This if statement will see if the "eighthRemainder1" can divided down anymore and if so it will keep on dividing "eighthRemainder1". Then it will slip out the answer into the console.
+//This if statement will see if the "eighthRemainder1" can divided down anymore and if so it will keep on dividing "eighthRemainder1". Then it will spit out the answer into the console.
+//Will show the answer in console.
 if (eighthRemainder1 > 0){
     let leftOvers = eighthRemainder1.toString(2);
     
@@ -61,14 +62,17 @@ if (eighthRemainder1 > 0){
     console.log(`The binary of ${decimalNumber} is ${leftOvers} ${eighthRemainder2}${seventhRemainder2}${sixthRemainder2}${fifthRemainder2} ${fourthRemainder2}${thirdRemainder2}${secondRemainder2}${firstRemainder2}`);
 
 }
-//This if statement will slip out the binary of the given decimal only if "eighthRemainder1" can't be divided down anymore.
+//This if statement will spit out the binary of the given decimal only if "eighthRemainder1" can't be divided down anymore.
+//Will show the answer in console.
 if (eighthRemainder1 == 0){
 
     console.log(`The binary of ${decimalNumber} is ${eighthRemainder2}${seventhRemainder2}${sixthRemainder2}${fifthRemainder2} ${fourthRemainder2}${thirdRemainder2}${secondRemainder2}${firstRemainder2}`);
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/*This line of code will take the given decimal number and then it will keep on dividing the given decimal number by 16 to it to a hexadecimal number.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+* This line of code will take the given decimal number and then it will keep on dividing the given decimal number by 16 to it to a hexadecimal number.
+* Will show the answer in the console.
 */
 
 console.log(`The hexadecimal of ${decimalNumber} is ${decimalNumber.toString(16)}`);
